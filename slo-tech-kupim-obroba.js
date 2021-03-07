1d80d07b2a5023f9fd423fccdb230efb6542f195
Franc Kebe <franc.kebe@gmail.com>
2021-03-07 16:02:54 +0100

    dodana skripta za slo-tech označitev K:Kupim

---
+++ slo-tech-kupim-obroba.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name     Unnamed Script 977453
+// @version  1
+// @grant    none
+// @require       http://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.js
+// ==/UserScript==
+
+// [1] skip all iframe
+if (window.self!=window.top) {return}
+
+$('tr[data-tip="K"]').css('border', '3px solid red');
+$('tr[data-tip="O"]').css('border', '1px solid blue');
